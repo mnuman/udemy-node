c6af7928ac32c5208a1ef1455c9397da893e72d7
Milco Numan <milco.numan@gmail.com>
2018-03-20 12:18:07 +0100

    Required my module, exported a function!

--- notes-node/app.js
+++ notes-node/app.js
@@ -6,5 +6,5 @@ const notes = require('./notes.js')
 
 // var user = os.userInfo();
 // fs.appendFile('greetings.txt', `Hello ${user.username}. You are ${notes.age}.`, (err) => { if (err) throw err; });
-var res = notes.addNote();
+var res = notes.add(5,42);
 console.log(res);
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,6 @@
 console.log('Starting notes.js');
 module.exports.age = 49;
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
+module.exports.add = (a,b) => {
+    return a+b;
 };
